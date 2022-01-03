3953b4fe094150208c070d5541841d978ed91b90
Oli <oliglegg@hotmail.co.uk>
2022-01-03 01:20:02 +0000

    printing basic functionality

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ mod server_status;
 
 use args::get_args;
 use messages::send_message;
-use server_status::ServerStatus;
 
 // https://wiki.vg/Server_List_Ping
 fn main() -> Result<(), Error> {
@@ -19,14 +18,16 @@ fn main() -> Result<(), Error> {
 
     let buff = send_message(&stream, &address)?;
     let utf8_json_string = String::from_utf8_lossy(&buff[..]);
-    let v: ServerStatus = serde_json::from_str(&utf8_json_string)?;
+    // let v: ServerStatus = serde_json::from_str(&utf8_json_string)?;
 
-    println!();
-    println!("Server: {}", v.description.text);
-    println!("Players: {}/{}", v.players.online, v.players.max);
-    println!();
+    println!("{}", utf8_json_string);
+    // println!();
+    // println!("Server: {}", v.description.text);
+    // println!("Players: {}/{}", v.players.online, v.players.max);
+    // println!();
     // println!("Version:");
     // println!("\tName: {}", v.version.name);
     // println!("\tProtocol: {}", v.version.protocol);
+
     Ok(())
 }
