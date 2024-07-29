43af8400d80dd1989e4105f582856fd124b9c76d
Oli Legg <oliglegg@hotmail.co.uk>
2024-07-29 21:49:00 +0100

    Fixing args bug

--- src/args.rs
+++ src/args.rs
@@ -18,10 +18,22 @@ pub fn get_args() -> Result<Address, std::io::Error> {
             url: args[1].to_string(),
             port: 25565,
         }),
-        3 => Ok(Address {
-            url: args[2].to_string(),
-            port: 25565,
-        }),
+        3 => {
+            let port = match args[2].parse::<u16>() {
+                Ok(p) => p,
+                Err(_) => {
+                    help();
+                    return Err(Error::new(
+                        ErrorKind::InvalidInput,
+                        "Port must be a valid number",
+                    ));
+                }
+            };
+            Ok(Address {
+                url: args[1].to_string(),
+                port,
+            })
+        }
         _ => {
             help();
             return Err(Error::new(ErrorKind::Other, "Bad Args"));
